0643a73f307b5565ce51a9ad5a3619e1281b91d0
TJ <TajjalleeAbid@gmail.com>
2021-04-10 00:45:32 +0400

    sca sdk clean up

--- app/src/main/java/com/tg/jitpacksample/MainActivity.kt
+++ app/src/main/java/com/tg/jitpacksample/MainActivity.kt
@@ -7,8 +7,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import com.tg.jitpack_lib.TestTgBobfSdk
-import tg.sdk.sca.presentation.ui.enrollment.TgDashboardActivity
-import tg.sdk.sca.tgbobf.TgBobfSdk
+//import tg.sdk.sca.presentation.ui.enrollment.TgDashboardActivity
+//import tg.sdk.sca.tgbobf.TgBobfSdk
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,23 +24,23 @@ class MainActivity : AppCompatActivity() {
 
         TestTgBobfSdk.init(application)
 
-        TgBobfSdk.init(application)
-
-        if (!TgBobfSdk.isUserEnrolled()) {
-            // fetch userToken and pass it as parameter
-            startActivity(
-                TgDashboardActivity.getEnrollmentLaunchIntent(
-                    context = this,
-                    userToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkiLCJpc3MiOiJodHRwczovL2F1dGhzZXJ2ZXIuYmFuay5jb20iLCJleHAiOjE2MTc3NDgxOTEsImlhdCI6MTYxNzc0NDU5MX0.C3wCSJ4b6ZxrCeSbNh3J1ZjK2E6nwJNUIXGx1yPwYCDg7sPPb9AVx5UR0tSjMoXIHxw0boLrht5oCNvIf-ZzEwQ_8rc7nGz7kZnWKY-ShtGjshGE01GHCMQugBgwcbtvoLg09Vrvfe64nWURFYNFgys3sDehKdJbkjSl661DzKOMDxiWXob1e-CU-IT4p-uqgcmI1S-Ir_FcW6qs8Qce1M--v4j7slifRh6Tl7ztDKPydzKt5viTtZzJNf6foZeGYmoNAOB4OTNmD9NztCbg7-t9lfypBt1sao3ZxKlAzMLuZdLQ1KzqCDJq7pbcBNm_-h5N7UqE1Jy6hI3k9FiRfQ"
-                )
-            )
-        } else {
-            startActivity(
-                TgDashboardActivity.getDashboardLaunchIntent(
-                    context = this
-                )
-            )
-        }
+//        TgBobfSdk.init(application)
+//
+//        if (!TgBobfSdk.isUserEnrolled()) {
+//            // fetch userToken and pass it as parameter
+//            startActivity(
+//                TgDashboardActivity.getEnrollmentLaunchIntent(
+//                    context = this,
+//                    userToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkiLCJpc3MiOiJodHRwczovL2F1dGhzZXJ2ZXIuYmFuay5jb20iLCJleHAiOjE2MTc3NDgxOTEsImlhdCI6MTYxNzc0NDU5MX0.C3wCSJ4b6ZxrCeSbNh3J1ZjK2E6nwJNUIXGx1yPwYCDg7sPPb9AVx5UR0tSjMoXIHxw0boLrht5oCNvIf-ZzEwQ_8rc7nGz7kZnWKY-ShtGjshGE01GHCMQugBgwcbtvoLg09Vrvfe64nWURFYNFgys3sDehKdJbkjSl661DzKOMDxiWXob1e-CU-IT4p-uqgcmI1S-Ir_FcW6qs8Qce1M--v4j7slifRh6Tl7ztDKPydzKt5viTtZzJNf6foZeGYmoNAOB4OTNmD9NztCbg7-t9lfypBt1sao3ZxKlAzMLuZdLQ1KzqCDJq7pbcBNm_-h5N7UqE1Jy6hI3k9FiRfQ"
+//                )
+//            )
+//        } else {
+//            startActivity(
+//                TgDashboardActivity.getDashboardLaunchIntent(
+//                    context = this
+//                )
+//            )
+//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
